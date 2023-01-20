d7be136f59cdcfdcba8c409e35a18589541e3ca7
cgreenblatt <cynthia_greenblatt@yahoo.com>
2023-01-20 15:05:46 -0800

    fix: Convert Posts component from class to function

--- app/components/Posts.js
+++ app/components/Posts.js
@@ -1,54 +1,37 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 import PropTypes from 'prop-types'
 import { fetchMainPosts } from '../utils/api'
 import Loading from './Loading'
 import PostsList from './PostsList'
 
-export default class Posts extends React.Component {
-  state = {
-    posts: null,
-    error: null,
-    loading: true,
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'success': return { posts: action.posts, error: '', loading: false };
+    case 'error': return { posts: null, error: action.message, loading: false };
+    case 'loading': return { posts: null, error: '', loading: true };
+    default: throw Error(`Unknown action ${action.type} was passed to the reducer`);
   }
-  componentDidMount() {
-    this.handleFetch()
-  }
-  componentDidUpdate(prevProps) {
-    if (prevProps.type !== this.props.type) {
-      this.handleFetch()
-    }
-  }
-  handleFetch () {
-    this.setState({
-      posts: null,
-      error: null,
-      loading: true
-    })
+}
 
-    fetchMainPosts(this.props.type)
-      .then((posts) => this.setState({
-        posts,
-        loading: false,
-        error: null
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loading: false
-      }))
+export default function Posts({ type }) { 
+  const [state, dispatch] = useReducer(reducer, { posts: null, error: '', loading: true });
+  useEffect(() => {
+    dispatch({ type: 'loading' });
+    fetchMainPosts(type)
+    .then((posts) => dispatch({ type: 'success', posts }))
+    .catch(({ message }) => dispatch({ type: 'error', message }))
+  }, [type])
+
+    console.log(state);
+  const { posts, error, loading } = state;
+
+  if (loading === true) {
+    return <Loading />
   }
-  render() {
-    const { posts, error, loading } = this.state
-
-    if (loading === true) {
-      return <Loading />
-    }
-
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
-
-    return <PostsList posts={posts} />
+  if (error) {
+    return <p className='center-text error'>{error}</p>
   }
+  return <PostsList posts={posts} />
 }
 
 Posts.propTypes = {
